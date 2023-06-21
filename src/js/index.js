d62c0ba5401fafd422284328f483bfd26ac0aa33
Cangre1 <suareznicolasm@gmail.com>
2023-06-21 13:20:23 -0300

    Ajustes Nico

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,3 @@
-//INICIO DE FUNCION 1//
-let service1 = document.getElementById("service1");
-let slide1 = document.getElementById("slide1");
-let slide2 = document.getElementById("slide2");
-
-if (service1) {
-  service1.addEventListener("click", (e) => {
-    slide2.classList.remove("text-lime-500");
-    slide2.classList.add("text-red-500");
-  });
-}
-//FIN DE FUNCION 1//
-
 // MENÚ MOBILE START //
 let menuHambur = document.getElementById("menu-hambur");
 let menumobile = document.getElementById("menu-mobile");
@@ -59,3 +46,80 @@ anchorLinks.forEach((link) => {
   });
 });
 // MENÚ MOBILE END //
+
+//SERVICIOS HOME//
+
+function mostrarParametro(event) {
+  // Obtiene el elemento <a> más cercano al elemento clicado
+  const linkElement = event.target.closest("a");
+
+  // Verifica si se encontró el elemento <a>
+  if (linkElement) {
+    // Obtiene la URL del enlace
+    const url = new URL(linkElement.href);
+
+    // Obtiene el valor del parámetro 'slide' de la URL
+    const slideParam = url.searchParams.get("slide");
+
+    // Muestra el parámetro en la consola
+    console.log("Parámetro slide:", slideParam);
+  }
+}
+
+// Obtén el valor del parámetro 'slide' de la URL
+const urlParams = new URLSearchParams(window.location.search);
+const slideParam = urlParams.get("slide");
+
+// Inicializa el slider
+const swiperDesk = new Swiper(".mySwiper", {
+  slidesPerView: 8,
+  simulateTouch: true,
+  spaceBetween: 10,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  on: {
+    init: function () {
+      // Verifica si se proporcionó el parámetro 'slide' y es un número válido
+      if (slideParam && !isNaN(slideParam)) {
+        // Convierte el valor del parámetro 'slide' a un índice de slide (resta 1)
+        const slideIndex = parseInt(slideParam) - 1;
+
+        // Verifica si el índice de slide es válido
+        if (slideIndex >= 0 && slideIndex < this.slides.length) {
+          // Cambia al slide correspondiente
+          this.slideTo(slideIndex);
+        }
+      }
+    },
+  },
+});
+
+// Obtén todas las diapositivas con la clase "slide-item"
+const slideItems = document.querySelectorAll(".slide-item");
+
+// Agrega un controlador de eventos "click" a cada diapositiva
+slideItems.forEach((slide) => {
+  slide.addEventListener("click", () => {
+    // Obtén el índice de la diapositiva haciendo referencia al atributo "data-index"
+    const slideIndex = parseInt(slide.dataset.index);
+
+    // Desplázate a la diapositiva correspondiente utilizando la función "slideTo" de Swiper
+    swiperDesk.slideTo(slideIndex);
+  });
+});
+
+swiperDesk.on("slideChange", function () {
+  var bloqueOculto = document.querySelectorAll(".section-detalle");
+
+  // Ocultar todos los bloques ocultos
+  bloqueOculto.forEach(function (bloque) {
+    bloque.style.display = "none";
+  });
+
+  // Mostrar el bloque correspondiente al índice activo
+  bloqueOculto[swiperDesk.activeIndex % 8].style.display = "block";
+});
